fd81939dad8ce76d32531239fc402ceaa8e63c42
tkimx7 <55491619+tkimx7@users.noreply.github.com>
2022-06-01 03:37:42 -0400

    Update README.R

--- README.R
+++ README.R
@@ -2,7 +2,7 @@
 ###### --- README --- ######################################################################################
 ############################################################################################################
 # 
-# --- I publish here a sample method of importing and processing data for some 16,000 stocks (as of Jun1, 2022) 
+# --- I publish here a sample method of importing and processing data for some 16,000 stocks (as of 4/29/2022) 
 # --- from Yahoo Finance via a parallel method in R. For readability purposes, the method's divided into a 
 # --- "header" file for all function declarations and a "getter" file to run these declarations in a parallel 
 # --- fashion. For every data frame for each ticker imported from Yahoo Finance, it standardizes all prices 
