25e86502c78850925af9f3763dc9fef648f03485
tkimx7 <55491619+tkimx7@users.noreply.github.com>
2022-06-01 02:32:15 -0400

    Update README.R

--- README.R
+++ README.R
@@ -2,12 +2,19 @@
 ###### --- README --- ######################################################################################
 ############################################################################################################
 # 
-# --- I publish here a sample method of importing and processing data for some 18,000 stocks from Yahoo Finance 
-# --- via a parallel method in R. For readability purposes, the method's divided into a "header" file for all 
-# --- function declarations and a "getter" file to run these declarations in a parallel fashion. For every 
-# --- data frame for each ticker imported from Yahoo Finance, it standardizes all prices into a index starting
-# --- at 100 (see the "indexConstruction" repository for the fundamentals of building an index). This data 
-# --- frame is then appended to other 
+# --- I publish here a sample method of importing and processing data for some 16,000 stocks (as of Jun1, 2022) 
+# --- from Yahoo Finance via a parallel method in R. For readability purposes, the method's divided into a 
+# --- "header" file for all function declarations and a "getter" file to run these declarations in a parallel 
+# --- fashion. For every data frame for each ticker imported from Yahoo Finance, it standardizes all prices 
+# --- into an index starting at 100 (see the "indexConstruction" repository to learn the fundamentals of 
+# --- building an index. Essentially, all stock price levels must be standardized to start at 100 for 
+# --- comparability purposes). In addition to stock prices, there are standardized prices for the S&P 500 index,
+# --- the crude oil index and the treasury yield indices for t-bills, notes and bonds. These serve as 
+# --- stastically significant independent variables for stock prediction models. I do not share the prediction 
+# --- model itself as it's proprietary.
+#
+# --- The results of this method are the stock price history and ggplot visual dashboards for each and every
+# --- ticker.
 #
 # --- This parallel method reduces the time of sequentially importing and processing data from 5 hours to 
 # --- 0.5 hours. It works for no more than 4 cores. Registering more cores significantly increases overhead 
